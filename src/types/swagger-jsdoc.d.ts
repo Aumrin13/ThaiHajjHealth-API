e0968eefe10757895ab869bb283d67b6ba2f912a
Aumrin13 <samoh_sp@hotmail.com>
2025-11-14 15:44:13 +0700

    Fix swagger-jsdoc TypeScript namespace declaration

--- src/types/swagger-jsdoc.d.ts
+++ src/types/swagger-jsdoc.d.ts
@@ -1,38 +1,40 @@
 declare module 'swagger-jsdoc' {
-  interface SwaggerDefinition {
-    openapi?: string;
-    info: {
-      title: string;
-      version: string;
-      description?: string;
-      contact?: {
-        name?: string;
-        email?: string;
-        url?: string;
+  namespace swaggerJsdoc {
+    interface SwaggerDefinition {
+      openapi?: string;
+      info: {
+        title: string;
+        version: string;
+        description?: string;
+        contact?: {
+          name?: string;
+          email?: string;
+          url?: string;
+        };
+        license?: {
+          name: string;
+          url?: string;
+        };
       };
-      license?: {
+      servers?: Array<{
+        url: string;
+        description?: string;
+      }>;
+      components?: any;
+      security?: any[];
+      tags?: Array<{
         name: string;
-        url?: string;
-      };
-    };
-    servers?: Array<{
-      url: string;
-      description?: string;
-    }>;
-    components?: any;
-    security?: any[];
-    tags?: Array<{
-      name: string;
-      description?: string;
-    }>;
+        description?: string;
+      }>;
+    }
+
+    interface Options {
+      definition: SwaggerDefinition;
+      apis: string[];
+    }
   }
 
-  interface Options {
-    definition: SwaggerDefinition;
-    apis: string[];
-  }
-
-  function swaggerJsdoc(options: Options): any;
+  function swaggerJsdoc(options: swaggerJsdoc.Options): any;
 
   export = swaggerJsdoc;
 }
